eb4dcafaf439301398146b4e2c3956c9828428e4
dcallif <dcallif22@gmail.com>
2020-03-24 15:38:50 -0600

    Signup users from UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 105 },
     uniqueness: { case_sensitive: false },
     format: { with: VALID_EMAIL_REGEX }
-    has_secure_password
+  has_secure_password
 end
