4a52465767b7a0fc4fd41e77ae0636b2fdc35954
dcallif <dcallif22@gmail.com>
2020-03-25 15:12:57 -0600

    Added action restrictions based off logged in user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except:[:index, :show]
+  before_action :require_same_user, only:[:edit, :update, :destroy]
 
   # GET /articles
   # GET /articles.json
@@ -25,7 +27,7 @@ class ArticlesController < ApplicationController
   # POST /articles.json
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     respond_to do |format|
       if @article.save
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
@@ -71,4 +73,11 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :description)
     end
+
+    def require_same_user
+      if current_user != @article.user
+        flash[:danger] = "You can only edit or delete your own articles"
+        redirect_to root_path
+    end
+  end
 end
