85f93cf9aef49c9c401386a37e1c2abba9ae7256
dcallif <dcallif22@gmail.com>
2020-03-25 13:32:49 -0600

    Added session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "There was an error with your login. Please try again."
+      render 'new'
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:success] = "You have logged out"
+    redirect_to root_path
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password_digest)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
