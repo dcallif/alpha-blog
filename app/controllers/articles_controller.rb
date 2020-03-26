9d96c3f3225b90403f0fbefa794c80ef69c57a1c
dcallif <dcallif22@gmail.com>
2020-03-25 21:15:46 -0600

    Added auth around showing article create button and minor styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -58,7 +58,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article.destroy
     respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
+      format.html { redirect_to articles_url, notice: 'Article was successfully deleted.' }
       format.json { head :no_content }
     end
   end
