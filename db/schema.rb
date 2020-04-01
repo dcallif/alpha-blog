9e34791dd0406faf914decb5ef45fe4454bdaf8b
dcallif <dcallif22@gmail.com>
2020-04-01 08:41:51 -0600

    Created article cateogry migration and base model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_173711) do
+ActiveRecord::Schema.define(version: 2020_04_01_143909) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
